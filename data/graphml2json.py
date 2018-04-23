cf4a22488033ba2665a97a5d57999b3e639dd822
Victor Pimenta <vfpimenta.93@gmail.com>
2018-04-23 19:11:21 -0300

    Updated graph

---
+++ data/graphml2json.py
@@ -0,0 +1,21 @@
+from networkx.readwrite import json_graph
+from optparse import OptionParser
+import networkx as nx
+import json
+import re
+
+parser = OptionParser()
+parser.add_option('-s', '--source', dest='source', type='str', help='Source file (graphml)', metavar='STRING')
+parser.add_option('-t', '--target', dest='target', type='str', help='Target file (json)', metavar='STRING')
+
+(options, args) = parser.parse_args()
+
+G=nx.read_graphml(options.source, str)
+data = json_graph.node_link_data(G)
+
+for node in data['nodes']:
+  str=node['id']
+  node['id']=[int(s) for s in str.split("n") if s.isdigit()][0]
+
+with open(options.target, 'w') as f:
+  json.dump(data, f, indent=4)
